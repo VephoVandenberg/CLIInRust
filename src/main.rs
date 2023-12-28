cfef6174452d630e53e9c9d8ec092260537c66a5
VephoVandenberg <azamatthebest.cs.py@gmail.com>
2023-12-28 17:57:12 +0300

    Added case sensitivity

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let conf = minigrep::Config::build(&args).unwrap_or_else(|err| {
+    let conf = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
