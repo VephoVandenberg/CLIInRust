2712446446a2874afa506adfd73dff025cceb38a
VephoVandenberg <azamatthebest.cs.py@gmail.com>
2024-01-06 13:28:31 +0300

    Added iterators

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Too few arguments");
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let query: String = args[1].clone();
-        let file_path = args[2].clone();
+        let query: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { query, file_path, ignore_case }) 
@@ -40,29 +48,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search_case_sensetive<'a>(query: & str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    
-    let mut results = Vec::new();
-    
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query))
+        .collect()
 }
 
 pub fn search<'a>(query: & str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    
-    for line in contents.lines() {
-        if line.contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(&query))
+        .collect()
 }
 
 
